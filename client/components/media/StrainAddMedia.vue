14fa26f5bf44bbf2556b574b3ac3ae3387eb4a5f
secret-super-star <mjone352@gmail.com>
2021-02-17 20:08:36 -0500

    add new icon

--- client/components/media/StrainAddMedia.vue
+++ client/components/media/StrainAddMedia.vue
@@ -1,11 +1,11 @@
 <template>
-    <span @click="addMedia = true" color="primary" text-color="#000" type="flat">
-        <img class="img-fluid h-50" src="/imgs/add.png" alt="Add Strain" />
+    <div @click="addMedia = true" color="primary" text-color="#000" type="flat">
+        <img class="img-fluid h-50" src="/imgs/strain_add_media.png" alt="Add Strain" />
 
         <vs-popup class="strains__popup media__add" type="border" title :active.sync="addMedia">
             <add-form :from="from" mode="add" :strain="strain_data.strain"></add-form>
         </vs-popup>
-    </span>
+    </div>
 </template>
 
 <script>
