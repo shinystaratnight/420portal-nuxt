cc151872c53c421164738fed70785d5220269f48
healer429 <mjone352@gmail.com>
2021-02-24 00:55:33 -0500

    remove throttle

--- app/Http/Controllers/StrainController.php
+++ app/Http/Controllers/StrainController.php
@@ -94,6 +94,8 @@ class StrainController extends Controller
     } else {
       $strainDetail = Strain::where('slug', $strain)->firstOrFail();
       $brand_array = User::whereType('brand')->pluck('id');
+
+      $strainDetail->menus = $strainDetail->menus->whereNotIn('portal_id', $brand_array)->all();
       $strain_menus = $strainDetail->menus->whereNotIn('portal_id', $brand_array)->pluck('media_id')->unique()->toArray();
       $strain_menus = array_filter($strain_menus);
 
