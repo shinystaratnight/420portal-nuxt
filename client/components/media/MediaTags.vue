eb38b009d3e5044eee7f343653f9bdd16b530199
secret-super-star <mjone352@gmail.com>
2021-02-28 20:10:06 -0500

    tagged popup profile image

--- client/components/media/MediaTags.vue
+++ client/components/media/MediaTags.vue
@@ -48,7 +48,8 @@
     >
       <p class="d-flex align-items-center">
         <a :href="'/' + item.username">
-          <img :src="serverUrl(item.profile_pic.url)" alt="Profile picture" />
+          <img :src="serverUrl(item.profile_pic.url)" alt="Profile picture" v-if="item.profile_pic" />
+          <img src="/imgs/default_sm.png" alt="Profile picture" v-else />
         </a>
         <a :href="'/' + item.username" class="username">{{ item.name }}</a>
       </p>
@@ -82,7 +83,8 @@
     >
       <p class="d-flex align-items-center">
         <a :href="'/' + item.username">
-          <img :src="serverUrl(item.profile_pic.url)" alt="Profile picture" />
+          <img :src="serverUrl(item.profile_pic.url)" alt="Profile picture" v-if="item.profile_pic" />
+          <img src="/imgs/default_sm.png" alt="Profile picture" v-else />
         </a>
         <span class="taged-company">
           <a :href="'/' + item.username" class="username">{{ item.name }}</a>
