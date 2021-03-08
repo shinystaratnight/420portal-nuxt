23a49e8f6b6ce4c5531b747ba0836300cfa115d8
phyzerbert <luckydev2288@gmail.com>
2021-03-09 00:34:52 +0900

    import brand

--- app/User.php
+++ app/User.php
@@ -276,6 +276,9 @@ class User extends Authenticatable implements JWTSubject //, MustVerifyEmail
     public function isOpen($open, $close, $timezone) {
         $fopen = Carbon::createFromFormat('h:i A', $open, $timezone);
         $fclose = Carbon::createFromFormat('h:i A', $close, $timezone);
+        if($fopen->isAfter($fclose)) {
+            $fclose->addDays(1);
+        }
         $currentTime = Carbon::now($timezone);
         if ($currentTime->between($fopen, $fclose)) {
             return 2;
