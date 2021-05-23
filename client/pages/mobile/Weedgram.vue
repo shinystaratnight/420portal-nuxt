9111c087dc26f56f4097294fc3fadab249529e82
Christopher Kim <star-atnight@outlook.com>
2021-05-23 08:48:38 +0800

    Add attributes to enable video-tag autoplay on iphone

--- client/pages/mobile/Weedgram.vue
+++ client/pages/mobile/Weedgram.vue
@@ -44,8 +44,8 @@
                     <div class="media_body">
                         <div class="slide_image" @click="user ? likeMedia(item) : () => false">
                             <img :src="serverUrl(item.url)" alt v-if="item.type == 'image'" />
-                            <video :src="serverUrl(item.url)" alt v-if="item.type == 'video' && ($route.params.start_index === index+1)"  onclick="this.paused ? this.play() : this.pause();" disablepictureinpicture controlslist="nodownload" autoplay playsinline></video>
-                            <video :src="serverUrl(item.url)" alt v-else-if="item.type == 'video'"  onclick="this.paused ? this.play() : this.pause();" disablepictureinpicture controlslist="nodownload" playsinline></video>
+                            <video :src="serverUrl(item.url)" alt v-if="item.type == 'video' && ($route.params.start_index === index+1)"  onclick="this.paused ? this.play() : this.pause();" disablepictureinpicture controlslist="nodownload" autoplay="autoplay" loop="loop" muted defaultMuted playsinline></video>
+                            <video :src="serverUrl(item.url)" alt v-else-if="item.type == 'video'"  onclick="this.paused ? this.play() : this.pause();" disablepictureinpicture controlslist="nodownload" autoplay="autoplay" loop="loop" muted defaultMuted playsinline></video>
                         </div>
                         <div class="menu-panel" v-if="item.menu && item.menu.is_active" :class="{'brand-menu': item.user.type == 'brand'}">
                             <p class="menu-category">
